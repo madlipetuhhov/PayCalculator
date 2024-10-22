ced62da7660c782d26c0a6f747be0c5ecc42a45e
Liis U. & Madli <madli.petuhhov89@gmail.com>
2024-10-22 12:14:35 +0300

    calcBasicExemption, II pillar, unempolyment Insurance and BigDecimal in use done

--- src/main/java/org/example/PayCalculator.java
+++ src/main/java/org/example/PayCalculator.java
@@ -1,46 +1,56 @@
 package org.example;
 
-import java.text.DecimalFormat;
+import java.math.BigDecimal;
 
 public class PayCalculator {
 
     public static final int ANNUAL_MAX_INCOME = 25_200;
     public static final int ANNUAL_MIN_INCOME = 14_400;
     public static final double MAX_INCOME_TAX = 0.1928;
+    public static final int MONTHLY_BASIC_EXEMPTION = 654;
 
-    public double calcSavingsPensionIIPillar(double grossSalary) {
-        System.out.println("II sammas " + (grossSalary * 2) / 100);
-        return (grossSalary * 2) / 100;
+    public BigDecimal calcSavingsPensionIIPillar(BigDecimal grossSalary) {
+        return grossSalary.multiply(new BigDecimal("2")).divide(new BigDecimal("100"));
     }
 
-    public double calcUnemploymentInsurancePayment(double grossSalary) {
-        System.out.println("tootus " + (grossSalary * 1.6) / 100);
-
-        return (grossSalary * 1.6) / 100;
+    public BigDecimal calcUnemploymentInsurancePayment(BigDecimal grossSalary) {
+        return grossSalary.multiply(new BigDecimal("1.6")).divide(new BigDecimal("100"));
     }
 
-    public double calcIncomeTax(double grossSalary) {
-        double annualGrossSalary = grossSalary * 12;
-        // aastatuluga kuni 14 400 eurot on maksuvaba tulu 7848 eurot aastas (kuni 1200 eur kuus),
-//        todo: siit edasi
-
-        //aastatulu kasvades 14 400 eurolt 25 200 euroni väheneb maksuvaba tulu vastavalt valemile:
-        // 7848 – 7848 ÷ 10 800 × (tulu summa – 14 400),
-        if (annualGrossSalary > ANNUAL_MIN_INCOME && annualGrossSalary < ANNUAL_MAX_INCOME)
-            return 654 - 0.72667 * (grossSalary - 1200);
-
-        // aastatuluga üle 25 200 euro on maksuvaba tulu 0 eurot (2100 eurot kuus kaasaarvatud) - 19.28%
-        if (annualGrossSalary >= ANNUAL_MAX_INCOME) return grossSalary * MAX_INCOME_TAX;
-        System.out.println("income tax  " + (grossSalary * 6.2) / 100);
-        return (grossSalary * 6.2) / 100;
+    public BigDecimal calcBasicExemption(BigDecimal grossSalary) {
+        BigDecimal annualGrossSalary = grossSalary.multiply(new BigDecimal("12"));
+        if (grossSalary.doubleValue() <= MONTHLY_BASIC_EXEMPTION) return grossSalary;
+        if (annualGrossSalary.doubleValue() <= ANNUAL_MIN_INCOME) return new BigDecimal(MONTHLY_BASIC_EXEMPTION);
+        if (annualGrossSalary.doubleValue() > ANNUAL_MIN_INCOME && annualGrossSalary.doubleValue() < ANNUAL_MAX_INCOME)
+            return new BigDecimal(MONTHLY_BASIC_EXEMPTION).subtract(new BigDecimal("0.72667").multiply(grossSalary.subtract(new BigDecimal("1200"))));
+        return new BigDecimal("0");
     }
-
-    public String calcNetSalary(double grossSalary) {
-        return roundOff(grossSalary - calcSavingsPensionIIPillar(grossSalary) - calcUnemploymentInsurancePayment(grossSalary) - calcIncomeTax(grossSalary));
-    }
-
-    public String roundOff(double result) {
-        DecimalFormat df = new DecimalFormat("#.00");
-        return df.format(result);
-    }
+//
+//    public BigDecimal calcIncomeTax(BigDecimal grossSalary) {
+//        BigDecimal annualGrossSalary = grossSalary * 12;
+//        // tulumaksu uldse ei voeta
+//
+//        // aastatuluga kuni 14 400 eurot on maksuvaba tulu 7848 eurot aastas (kuni 1200 eur kuus),
+//
+//        //aastatulu kasvades 14 400 eurolt 25 200 euroni väheneb maksuvaba tulu vastavalt valemile:
+//        // 7848 – 7848 ÷ 10 800 × (tulu summa – 14 400),
+////        tulumaksu arvutamise lisamine ka vaja teha
+////        todo: kas me peaks arvestama ka seda, kui tootaja soovib kindlas summas tulumaksuvaba miinimumi
+//        if (annualGrossSalary > ANNUAL_MIN_INCOME && annualGrossSalary < ANNUAL_MAX_INCOME)
+//            return (MONTHLY_BASIC_EXEMPTION - 0.72667 * (grossSalary - 1200)) + MONTHLY_BASIC_EXEMPTION;
+////            return 7848 - (7848 / 10800) * (annualGrossSalary - ANNUAL_MIN_INCOME);
+//
+//        // aastatuluga üle 25 200 euro on maksuvaba tulu 0 eurot (2100 eurot kuus kaasaarvatud) - 19.28%
+//        if (annualGrossSalary >= ANNUAL_MAX_INCOME) return grossSalary * MAX_INCOME_TAX;
+//        System.out.println("income tax  " + (grossSalary * 6.2) / 100);
+//        return (grossSalary * 6.2) / 100;
+//    }
+//
+//    public BigDecimal calcNetSalary(BigDecimal grossSalary) {
+//        return roundOff(grossSalary - calcSavingsPensionIIPillar(grossSalary) - calcUnemploymentInsurancePayment(grossSalary) - calcIncomeTax(grossSalary));
+//    }
+//
+//    public BigDecimal roundOff(BigDecimal result) {
+//        return new BigDecimal("12.3456", new MathContext(2, RoundingMode.HALF_UP));
+//    }
 }
--- src/test/java/org/example/PayCalculatorTest.java
+++ src/test/java/org/example/PayCalculatorTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PayCalculatorTest {
@@ -9,47 +11,55 @@ class PayCalculatorTest {
 
     @Test
     void calcSavingsPensionIIPillar() {
-        assertEquals(20.00, calc.calcSavingsPensionIIPillar(1000));
-//        assertEquals(23.33, calc.calcSavingsPensionIIPillar(1166.665));
+        assertEquals(new BigDecimal("20"), calc.calcSavingsPensionIIPillar(new BigDecimal("1000")));
+        assertEquals(new BigDecimal("40"), calc.calcSavingsPensionIIPillar(new BigDecimal("2000")));
     }
 
     @Test
     void calcUnemploymentInsurancePayment() {
-        assertEquals(16.00, calc.calcUnemploymentInsurancePayment(1000));
-//        assertEquals(16.00, calc.calcUnemploymentInsurancePayment(grossSalary));
+        assertEquals(new BigDecimal("16.0"), calc.calcUnemploymentInsurancePayment(new BigDecimal("1000")));
+        assertEquals(new BigDecimal("32.0"), calc.calcUnemploymentInsurancePayment(new BigDecimal("2000")));
     }
 
     @Test
-    void calcIncomeTax() {
-        assertEquals(62.00, calc.calcIncomeTax(1000));
+    void calcBasicExemption() {
+        assertEquals(new BigDecimal("300.0"), calc.calcBasicExemption(new BigDecimal("300.0")));
+        assertEquals(new BigDecimal("654.0"), calc.calcBasicExemption(new BigDecimal("654.0")));
+        assertEquals(new BigDecimal("654"), calc.calcBasicExemption(new BigDecimal("1000.0")));
+        assertEquals(new BigDecimal("654"), calc.calcBasicExemption(new BigDecimal("1200.0")));
+        assertEquals(new BigDecimal("653.273330"), calc.calcBasicExemption(new BigDecimal("1201.0")));
+        assertEquals(new BigDecimal("0.723670"), calc.calcBasicExemption(new BigDecimal("2099.0")));
+        assertEquals(BigDecimal.ZERO, calc.calcBasicExemption(new BigDecimal("2100.0")));
+        assertEquals(BigDecimal.ZERO, calc.calcBasicExemption(new BigDecimal("4000.0")));
     }
-
-    @Test
-    void calcNetSalaryUpTo14400() {
-//  aastatuluga kuni 14 400 eurot on maksuvaba tulu 7848 eurot aastas
-        assertEquals("716.91", calc.calcNetSalary(760));
-        assertEquals("902.00", calc.calcNetSalary(1000));
-        assertEquals("1056.24", calc.calcNetSalary(1200));
-    }
-
-    @Test
-    void calcNetSalaryUpTo25K() {
-
-        assertEquals("1056.86", calc.calcNetSalary(1201));
-    }
-
-    @Test
-    void calcNetSalaryAbove25K() {
-        assertEquals("1619.52", calc.calcNetSalary(2100));
-        assertEquals("3084.80", calc.calcNetSalary(4000));
-    }
-
-    @Test
-    void roundOff() {
-        assertEquals("222.00", calc.roundOff(221.999));
-        assertEquals("221.58", calc.roundOff(221.579));
-        assertEquals("221.58", calc.roundOff(221.58));
-        assertEquals("221.06", calc.roundOff(221.055));
-        assertEquals("221.05", calc.roundOff(221.050));
-    }
+//
+////    todo> income tax kontroll
+//
+//    @Test
+//    void calcNetSalaryUpTo14400() {
+//        assertEquals("716.91", calc.calcNetSalary(760));
+//        assertEquals("902.00", calc.calcNetSalary(1000));
+//        assertEquals("1056.24", calc.calcNetSalary(1200));
+//    }
+//
+//    @Test
+//    void calcNetSalaryUpTo25K() {
+//
+//        assertEquals("1056.86", calc.calcNetSalary(1201));
+//    }
+//
+//    @Test
+//    void calcNetSalaryAbove25K() {
+//        assertEquals("1619.52", calc.calcNetSalary(2100));
+//        assertEquals("3084.80", calc.calcNetSalary(4000));
+//    }
+//
+//    @Test
+//    void roundOff() {
+//        assertEquals("222.00", calc.roundOff(221.999));
+//        assertEquals("221.58", calc.roundOff(221.579));
+//        assertEquals("221.58", calc.roundOff(221.58));
+//        assertEquals("221.06", calc.roundOff(221.055));
+//        assertEquals("221.05", calc.roundOff(221.050));
+//    }
 }
