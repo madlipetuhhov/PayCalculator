721cd56649bfb68a5ab8874fe49a9f364d1452c7
Liis U. & Madli <madli.petuhhov89@gmail.com>
2024-10-22 14:23:33 +0300

    NetSalary calculations done

--- src/main/java/org/example/PayCalculator.java
+++ src/main/java/org/example/PayCalculator.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class PayCalculator {
 
     public static final int ANNUAL_MAX_INCOME = 25_200;
     public static final int ANNUAL_MIN_INCOME = 14_400;
-    public static final double MAX_INCOME_TAX = 0.1928;
+    public static final double MAX_INCOME_TAX = 0.2;
     public static final int MONTHLY_BASIC_EXEMPTION = 654;
 
     public BigDecimal calcSavingsPensionIIPillar(BigDecimal grossSalary) {
@@ -22,35 +23,30 @@ public class PayCalculator {
         if (grossSalary.doubleValue() <= MONTHLY_BASIC_EXEMPTION) return grossSalary;
         if (annualGrossSalary.doubleValue() <= ANNUAL_MIN_INCOME) return new BigDecimal(MONTHLY_BASIC_EXEMPTION);
         if (annualGrossSalary.doubleValue() > ANNUAL_MIN_INCOME && annualGrossSalary.doubleValue() < ANNUAL_MAX_INCOME)
-            return new BigDecimal(MONTHLY_BASIC_EXEMPTION).subtract(new BigDecimal("0.72667").multiply(grossSalary.subtract(new BigDecimal("1200"))));
+            return new BigDecimal(MONTHLY_BASIC_EXEMPTION)
+                    .subtract(new BigDecimal("0.72667")
+                            .multiply(grossSalary
+                                    .subtract(new BigDecimal("1200"))));
         return new BigDecimal("0");
     }
-//
-//    public BigDecimal calcIncomeTax(BigDecimal grossSalary) {
-//        BigDecimal annualGrossSalary = grossSalary * 12;
-//        // tulumaksu uldse ei voeta
-//
-//        // aastatuluga kuni 14 400 eurot on maksuvaba tulu 7848 eurot aastas (kuni 1200 eur kuus),
-//
-//        //aastatulu kasvades 14 400 eurolt 25 200 euroni väheneb maksuvaba tulu vastavalt valemile:
-//        // 7848 – 7848 ÷ 10 800 × (tulu summa – 14 400),
-////        tulumaksu arvutamise lisamine ka vaja teha
-////        todo: kas me peaks arvestama ka seda, kui tootaja soovib kindlas summas tulumaksuvaba miinimumi
-//        if (annualGrossSalary > ANNUAL_MIN_INCOME && annualGrossSalary < ANNUAL_MAX_INCOME)
-//            return (MONTHLY_BASIC_EXEMPTION - 0.72667 * (grossSalary - 1200)) + MONTHLY_BASIC_EXEMPTION;
-////            return 7848 - (7848 / 10800) * (annualGrossSalary - ANNUAL_MIN_INCOME);
-//
-//        // aastatuluga üle 25 200 euro on maksuvaba tulu 0 eurot (2100 eurot kuus kaasaarvatud) - 19.28%
-//        if (annualGrossSalary >= ANNUAL_MAX_INCOME) return grossSalary * MAX_INCOME_TAX;
-//        System.out.println("income tax  " + (grossSalary * 6.2) / 100);
-//        return (grossSalary * 6.2) / 100;
-//    }
-//
-//    public BigDecimal calcNetSalary(BigDecimal grossSalary) {
-//        return roundOff(grossSalary - calcSavingsPensionIIPillar(grossSalary) - calcUnemploymentInsurancePayment(grossSalary) - calcIncomeTax(grossSalary));
-//    }
-//
-//    public BigDecimal roundOff(BigDecimal result) {
-//        return new BigDecimal("12.3456", new MathContext(2, RoundingMode.HALF_UP));
-//    }
+
+    public BigDecimal calcIncomeTax(BigDecimal grossSalary) {
+        return grossSalary
+                .subtract(calcSavingsPensionIIPillar(grossSalary))
+                .subtract(calcUnemploymentInsurancePayment(grossSalary))
+                .subtract(calcBasicExemption(grossSalary))
+                .multiply(new BigDecimal(MAX_INCOME_TAX));
+    }
+
+    public BigDecimal calcNetSalary(BigDecimal grossSalary) {
+        return roundOff(
+                grossSalary
+                        .subtract(calcSavingsPensionIIPillar(grossSalary))
+                        .subtract(calcUnemploymentInsurancePayment(grossSalary))
+                        .subtract(calcIncomeTax(grossSalary)));
+    }
+
+    public BigDecimal roundOff(BigDecimal result) {
+        return result.setScale(2, RoundingMode.HALF_UP);
+    }
 }
